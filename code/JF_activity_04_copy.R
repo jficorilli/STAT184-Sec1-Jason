0c10e875e54de1924cba1484fbf3bb7ac598a9bb
jficorilli <156146198+jficorilli@users.noreply.github.com>
2024-12-02 19:32:19 -0500

    Create JF_activity_04_copy.R
    Added my R code file from Activity 04

---
+++ code/JF_activity_04_copy.R
@@ -0,0 +1,5 @@
+galton_tidy_df <- read.csv("C:/Users/jason/Desktop/Locally saved school coding/STAT 184/galton_tidy_table.csv")
+View(galton_tidy_df)
+
+armed_forces_df <- read.csv("C:/Users/jason/Desktop/Locally saved school coding/STAT 184/US_armed_forces.csv")
+View(armed_forces_df)
